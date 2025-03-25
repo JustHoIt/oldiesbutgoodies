417a40c4a1f633f1194ef89aba785b04c6731deb
JustHo <zalop3128@kakao.com>
2025-03-26 03:23:16 +0900

    Initial commit

---
+++ src/main/java/com/hm/oldiesbutgoodies/OldiesbutgoodiesApplication.java
@@ -0,0 +1,13 @@
+package com.hm.oldiesbutgoodies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldiesbutgoodiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OldiesbutgoodiesApplication.class, args);
+    }
+
+}
