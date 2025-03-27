ef80f2450f35c5b658423923da6511854d0dcce8
JustHo <zalop3128@kakao.com>
2025-03-27 22:29:51 +0900

    User Entity 작성

---
+++ src/main/java/com/hm/oldiesbutgoodies/entity/User.java
@@ -0,0 +1,55 @@
+package com.hm.oldiesbutgoodies.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    private String address;
+    private String profileImg;
+    private String birth_date;
+    private String gender;
+    private String socialLoginType;
+    private String status;
+
+    // @Column(updatable = false) 처음 생성된 값(예: createdAt)이 변경되지 않도록 보호
+    // JPA가 UPDATE 쿼리를 실행할 때 해당 컬럼을 제외, 데이터의 무결성 유지
+    @Column(updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    // 자동으로 updatedAt 값을 갱신 → 매번 updatedAt = LocalDateTime.now();를 직접 호출할 필요 X
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
